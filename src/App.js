edb44fef0ae9b4019b5941b748b036fd159cd56b
Bashir Shallah <bashir.shallah@gmail.com>
2018-12-08 08:11:59 +0300

    handle conditions before template.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
+    const age = 10;
+    let ageTemplate = '';
+
+    if(age > 5){
+      ageTemplate = <h1>you are older than 5</h1>;
+    } else {
+      ageTemplate = <p>you are not older than 5</p>;
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {ageTemplate}
       </div>
     );
   }
